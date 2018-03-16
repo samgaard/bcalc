38cf2b1bc11b1a02cee1fbfd0fe0b528e60d1713
Sam Gaard <sam@fruition.net>
2018-03-15 23:34:31 -0600

    custom chart
    created custom chart based of view query and customized highcharts js

--- drupal/web/modules/custom/bcalc/src/Controller/Homepage.php
+++ drupal/web/modules/custom/bcalc/src/Controller/Homepage.php
@@ -3,8 +3,8 @@
 namespace Drupal\bcalc\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
 use Drupal\views\Views;
-use Drupal\Core\Render\Markup;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 
@@ -16,12 +16,12 @@ class Homepage extends ControllerBase {
   /**
    * Build.
    *
-   * @return string
-   *   Return Hello string.
+   * @return array
+   *   Return homepage theme array.
    */
   public function home() {
 
-    $tabs_content = buildHomepageTabs();
+    $tabs_content = $this->buildHomepageTabs();
 
     return [
       '#theme' => 'homepage',
@@ -32,73 +32,112 @@ class Homepage extends ControllerBase {
       ]
     ];
   }
+  private function buildHomepageTabs() {
 
-}
+    $items = [];
 
-function buildHomepageTabs() {
+    $tabs_content = [];
 
+    $active = TRUE;
+    for($i=1;$i<=12;$i++) {
 
-  $items = [];
+      $month = str_pad($i, 2, '0', STR_PAD_LEFT);
 
-  $tabs_content = [];
+      $view = Views::getView('line_items');
+      $view->setDisplay('block_1');
+      $view->setArguments(array('2018' . $month));
+      $viewRendered = $view->render();
+      if($viewRendered['#rows']) {
+        \Drupal::service('renderer')->render($viewRendered);
 
-  $active = TRUE;
-  for($i=1;$i<=12;$i++) {
+        $dateObj = \DateTime::createFromFormat('!m', $i);
+        $monthName = $dateObj->format('F');
 
-    $month = str_pad($i, 2, '0', STR_PAD_LEFT);
+        //CHART
+        $chart = $this->buildChart('2018' . $month);
 
-    $view = Views::getView('line_items');
-    $view->setDisplay('block_1');
-    $view->setArguments(array('2018' . $month));
-    $viewRendered = $view->render();
-    if($viewRendered['#rows']) {
-      \Drupal::service('renderer')->render($viewRendered);
+        $tabs_content[] = [
+          'active' => $active,
+          'month_name' => strtolower($monthName),
+          'content' => $viewRendered['#markup'],
+          'edit_arg' => '2018-' . $dateObj->format('m'),
+          'chart' => $chart
+        ];
 
-      $dateObj = \DateTime::createFromFormat('!m', $i);
-      $monthName = $dateObj->format('F');
+        $url = Url::fromUserInput('#' . strtolower($monthName), ['attributes' => ['data-toggle' => 'tab']]);
+        $link = Link::fromTextAndUrl(t($monthName), $url)->toString();
 
-      //CHART
-      $chart_view = Views::getView('line_items');
-      $chart_view->setDisplay('block_2');
-      $chart_view->setArguments(array('2018' . $month));
-      $chart_viewRendered = $chart_view->render();
-      if($chart_viewRendered['#rows']) {
-        \Drupal::service('renderer')->render($chart_viewRendered);
-        $chart = $chart_viewRendered['#markup'];
-      } else {
-        $chart = '';
-      }
+        if ($active) {
+          $items[] = ['#markup' => $link, '#wrapper_attributes' => ['class' => 'active']];
+        } else {
+          $items[] = $link;
+        }
 
-      $tabs_content[] = [
-        'active' => $active,
-        'month_name' => strtolower($monthName),
-        'content' => $viewRendered['#markup'],
-        'edit_arg' => '2018-' . $dateObj->format('m'),
-        'chart' => $chart
-      ];
-      
-      $url = Url::fromUserInput('#' . strtolower($monthName), ['attributes' => ['data-toggle' => 'tab']]);
-      $link = Link::fromTextAndUrl(t($monthName), $url)->toString();
-
-      if ($active) {
-        $items[] = ['#markup' => $link, '#wrapper_attributes' => ['class' => 'active']];
-      } else {
-        $items[] = $link;
-      }
-
-      if($active) {
-        $active = FALSE;
+        if($active) {
+          $active = FALSE;
+        }
       }
     }
+
+    $list = [
+      '#theme' => 'item_list',
+      '#items' => $items,
+      '#attributes' => [
+        'class' => 'nav nav-tabs'
+      ]
+    ];
+
+    return ['tabs' => $list, 'content' => $tabs_content];
   }
 
-  $list = [
-    '#theme' => 'item_list',
-    '#items' => $items,
-    '#attributes' => [
-      'class' => 'nav nav-tabs'
-    ]
-  ];
+  private function buildChart($year_month) {
+    $connection = Database::getConnection();
+    $query = "SELECT parent_term.name AS parent_name, SUM(amount.field_amount_value) AS amount, MIN(nfd.nid) AS nid, MIN(catdata.tid) AS cat_tid, MIN(parent_term.tid) AS parent_tid
+FROM 
+{node_field_data} nfd
+LEFT JOIN {node__field_category} nfc ON nfd.nid = nfc.entity_id
+LEFT JOIN {taxonomy_term_field_data} catdata ON nfc.field_category_target_id = catdata.tid
+LEFT JOIN {taxonomy_term_hierarchy} h ON catdata.tid = h.tid
+LEFT JOIN {taxonomy_term_field_data} parent_term ON h.parent = parent_term.tid
+LEFT JOIN {node__field_amount} amount ON nfd.nid = amount.entity_id
+LEFT JOIN {node__field_trans_date} transdate ON nfd.nid = transdate.entity_id 
+WHERE ((DATE_FORMAT(transdate.field_trans_date_value, '%Y%m') = :year_date)) 
+AND ((nfd.status = '1') 
+AND (nfd.type IN ('line_item')) 
+AND (amount.field_amount_value IS NOT NULL) 
+AND (parent_term.name IS NOT NULL))
+GROUP BY parent_name";
 
-  return ['tabs' => $list, 'content' => $tabs_content];
+    $results = $connection->query($query, [':year_date' => $year_month])->fetchAll();
+
+    $categories = [];
+    $seriesData = [];
+    $numbers = [];
+
+    foreach ($results as $key => $result) {
+      $categories[] = $result->parent_name;
+      $numbers[] = ['name' => $result->parent_name, 'y' => (int) number_format($result->amount, 0)];
+    }
+    $seriesData[] = ["data" => $numbers];
+
+    $options = [];
+    $options['title'] = t('Chart title');
+    $options['type'] = 'pie';
+    $options['yaxis_title'] = t('Y-Axis');
+    $options['yaxis_min'] = '';
+    $options['yaxis_max'] = '';
+    $options['xaxis_labels_rotation'] = 0;
+    $options['xaxis_title'] = t('X-Axis');
+    // Sample data format.
+
+    return  [
+      '#theme' => 'charts_api_example',
+      '#library' => 'highcharts',
+      '#categories' => $categories,
+      '#seriesData' => $seriesData,
+      '#options' => $options,
+    ];
+
+  }
 }
+
